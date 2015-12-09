3ed7e31dabc0092610175eb0776e437a3a5c2ec1
Jia Fu Low <jia.fu.low@cern.ch>
2015-12-08 20:29:09 -0600

    Update project testbeam_4a/software

--- projects/testbeam_4a/software/linux_skiroc_sc_test.c
+++ projects/testbeam_4a/software/linux_skiroc_sc_test.c
@@ -30,7 +30,7 @@ inline unsigned int gpio_read(void *gpio_base, unsigned int offset)
 
 int main()
 {
-	int i;
+	int i, j;
     int uio0_fd;
     const char *uio0_d = "/dev/uio0";
     void *uio0_ptr;
@@ -55,12 +55,18 @@ int main()
     source = (unsigned *) malloc(SKIROC_MAP_SIZE);
     destination = (unsigned *) uio0_ptr;
 
+    unsigned word;
+
     // Create twenty 32-bit words to encode 616 bits
+    printf("Create words...\n");
     for (i=0; i<NWORDS; i++) {
     	//*(source+i) = i;
 
-    	unsigned word = i;
+    	j = i%16;  // a hex char
+    	word = (j<<28) | (j<<24) | (j<<20) | (j<<16) | (j<<12) | (j<<8) | (j<<4) | (j<<0);  // concat 8 hex chars
     	*(source+i) = word;
+
+    	printf("32-bit word %2d: 0x%08x\n", i, word);
     }
 
     // Write
@@ -69,9 +75,10 @@ int main()
     }
 
     // Read
+    printf("\nRead words...\n");
     for (i=0; i<NWORDS; i++) {
-		unsigned word = gpio_read(destination, i*sizeof(unsigned));
-		printf("32-bit word %2d: 0x%02x\n", i, word);
+    	word = gpio_read(destination, i*sizeof(unsigned));
+    	printf("32-bit word %2d: 0x%08x\n", i, word);
     }
 
     printf("-- Exiting main() --\n");
